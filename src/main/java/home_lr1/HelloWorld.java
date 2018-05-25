56570970c1903c04b35965243c94fedb0e6fea20
ychernikov <ychernikov@playtika.com>
2018-05-25 17:24:08 +0300

    second commit

---
+++ src/main/java/home_lr1/HelloWorld.java
@@ -0,0 +1,10 @@
+package home_lr1;
+
+/**
+ * Created by admin on 5/25/2018.
+ */
+public class HelloWorld {
+    public static void main (String[]args){
+        System.out.println("Hello World!");
+    }
+}
